8c33c45a97da67dd4a8bc4e4aed1c0c5463006b2
张明凯 <zhangmingkai19892013@gmail.com>
2015-05-18 22:09:58 +0800

    增加了21章内容

---
+++ Chapte-20/读书笔记.js
@@ -0,0 +1,66 @@
+var person={
+	name:"Nico",
+	ageL:29
+};
+
+json的表示方式如下
+
+{
+	"name":"Nico",
+	"age":29,
+	"school":{
+		"name":"Merr",
+		"location":"Chicgo"
+	}
+	
+}
+
+json的数组表示方式：
+
+[
+	{  
+	    "name":"Nico",
+		"age":29,
+		"school":{
+			"name":"Merr",
+			"location":"Chicgo"
+		}
+
+	},
+	{
+		"name":"Mike",
+		"age":21,
+		"school":{
+			"name":"DC",
+			"location":"Chicgo"
+		}
+
+	},
+	{
+		"name":"Click",
+		"age":22,
+		"school":{
+			"name":"DC",
+			"location":"Chicgo"
+		}
+
+	}
+]
+
+
+JSON对象的两个函数：
+
+JSON.stringify(js对象,[过滤器])
+
+比如当有个js对象
+
+var person={
+		name:"Mike",
+		age:21,
+		school:{
+			name:"DC",
+			location:"Chicgo"
+		}
+	}
+
+var jsonText=JSON.stringify(person)
