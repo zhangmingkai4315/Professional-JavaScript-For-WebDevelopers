0cd3446ad1126b4ff4f5208a8cc55ad0c10a1b7e
张明凯 <zhangmingkai19892013@gmail.com>
2015-05-18 21:35:38 +0800

    增加十六章的内容

--- Chapte-14/读书笔记.js
+++ Chapte-14/读书笔记.js
@@ -28,7 +28,7 @@ EventUtil.addHandler(textBox,"blur",function(event){
 
 3. <textarea type="text" maxlength="50" size="25" value="text input area">
 
-4.  屏蔽字符串输入
+4.  屏蔽字符输入
 
 EventUtil.addHandler(textBox,"keypress",function(event){
 	event=EventUtil.getEvent(event);
@@ -43,3 +43,28 @@ EventUtil.addHandler(textBox,"keypress",function(event){
 
 
 });
+
+
+
+5. required text="email" text="url"
+   pattern="\d+"
+checkValidity()检查表单元素是否有效
+依据为各种默认的和要求的检查项：比如requried number pattern 
+
+
+6. 对于selection 动态的增加和删除可使用方法
+
+add remove 
+
+var newOption=new Option("OPTION TEXT","OPTION VALUE");
+selectbox.add(newOption,undefined);]]]
+
+
+7.  表单元素序列化
+8. 富文本框
+
+实现方式  1. 可以使用内嵌的iframe 并将其载入时设置designMode="on"
+	    2. 可以使用任何的元素，并将其contentEditable设置为true
+
+
+提交到服务器的是一段html代码 需要获取innerHTML代码保存到字符串
